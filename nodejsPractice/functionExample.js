b2a5dd911b61f0987cda02bcc08f24bedc9fb1ec
Angad <angad@hotmail.co.nz>
2022-07-16 15:10:06 +1200

    sentiment 16/7/22

--- nodejsPractice/functionExample.js
+++ nodejsPractice/functionExample.js
@@ -14,17 +14,25 @@
     return a*b;
   }
 
+  let concac = (inputName) =>{
+    return "Hello! " + inputName;
+  }
+
   let x = add1(1,2);
   let y = subtract1 (3,7);
   let z = multiply1 (3,3);
   let z1 = divide1 (5,3);
-  
+  let name = concac("Bob Ross");
+
   console.log(x);
   console.log(y);
   console.log(z);
   console.log(z1);
   console.log("");
-  const readline = require('readline').createInterface({
+  console.log(name);
+  console.log("");
+
+  /* const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
   });
@@ -32,4 +40,4 @@
   readline.question(`What's your name?`, name => {
     console.log(`Hi ${name}!`);
     readline.close();
-  });
+  }); */
